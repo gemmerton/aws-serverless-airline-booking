2d65e673d883ecb8bce1d7614e63b071705b1d91
Gary Emmerton <gemmerto@amazon.com>
2021-04-08 21:37:50 +0100

    Pinpoint analytics added

--- src/frontend/aws-exports.js.no-analytics.js
+++ src/frontend/aws-exports.js.no-analytics.js
@@ -10,9 +10,7 @@ const awsmobile = {
     "oauth": {},
     "aws_appsync_graphqlEndpoint": "https://v7vcyxmyt5be7n7bi6zwcdybyu.appsync-api.eu-west-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-1",
-    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_mobile_analytics_app_id": "2678f33c848f453b9027d1e7b1ff6197",
-    "aws_mobile_analytics_app_region": "eu-west-1"
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
