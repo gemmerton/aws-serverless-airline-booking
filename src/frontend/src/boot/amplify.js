59d2b364545b72528209f3e4b17990a82e24ba20
Gary Emmerton <gemmerto@amazon.com>
2021-04-09 09:17:12 +0100

    re-added analytics

--- src/frontend/src/boot/amplify.js
+++ src/frontend/src/boot/amplify.js
@@ -1,11 +1,12 @@
 import Amplify from '@aws-amplify/core'
 import { Auth } from '@aws-amplify/auth'
-//import { Analytics } from '@aws-amplify/analytics'
+import Analytics from '@aws-amplify/analytics'
+//import { Analytics } from '@aws-amplify'
 import awsconfig from '../../aws-exports'
 import '@aws-amplify/ui-vue'
 
 Amplify.configure(awsconfig)
 Auth.configure(awsconfig)
-//Analytics.configure(awsconfig)
+Analytics.configure(awsconfig)
 
 Amplify.Logger.LOG_LEVEL = process.env.LOG_LEVEL || 'INFO'
