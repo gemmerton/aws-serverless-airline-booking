83f8aaa235d270d451e781a2f681ba1a3f39a647
Gary Emmerton <gemmerto@amazon.com>
2021-04-12 20:50:58 +0100

    added search analytics 3

--- src/frontend/src/pages/Search.vue
+++ src/frontend/src/pages/Search.vue
@@ -242,14 +242,14 @@ export default {
      * search method collects form data, create queryStrings, and redirects to Search Results view
      */
     search() {
+      await Analytics.record({
+        name: 'search',
+        attributes: {
+          departure: this.departureCity.code,
+          arrival: this.arrivalCity.code
+        }
+      });
       this.$router.push({
-        await Analytics.record({
-          name: 'search',
-          attributes: {
-            departure: this.departureCity.code,
-            arrival: this.arrivalCity.code
-          }
-        });
         name: 'searchResults',
         query: {
           date: date.formatDate(this.departureDate, 'YYYY-MM-DD'),
