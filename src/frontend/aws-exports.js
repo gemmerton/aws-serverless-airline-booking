134c2f20ea362747fc133894974a23d80b00a9be
Gary Emmerton <gemmerto@amazon.com>
2021-04-14 10:05:01 +0100

    updated exports.js

--- src/frontend/aws-exports.js
+++ src/frontend/aws-exports.js
@@ -11,8 +11,8 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://mftperxncvhordrvmzo2pw4fcu.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_mobile_analytics_app_id": "2ffe83fa7ae04afb96f67543992974bd",
-    "aws_mobile_analytics_app_region": "eu-west-1"
+    "aws_mobile_analytics_app_id": "924cdf4c781d4e779ce657ab61117934",
+    "aws_mobile_analytics_app_region": "eu-west-2"
 };
 
 
